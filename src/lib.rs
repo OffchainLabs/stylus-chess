423c8465b2d7809c5d5c3274fea536c2d7f23b58
Chris Cordle <chrisco255@protonmail.com>
2024-02-20 12:41:46 -0700

    deriving game state from single 32 byte value

--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,10 @@ extern crate alloc;
 #[global_allocator]
 static ALLOC: mini_alloc::MiniAlloc = mini_alloc::MiniAlloc::INIT;
 
-use alloy_primitives::{Address, U8};
+use alloy_primitives::{hex, Address, Uint, B256, U64, U8};
 use chess_engine::{Board, BoardBuilder, Color, Evaluate, Move, Piece, Position};
 
+use stylus_sdk::console;
 /// Import the Stylus SDK along with alloy primitive types for use in our program.
 use stylus_sdk::{alloy_primitives::U256, msg, prelude::*, storage::StorageU8};
 
@@ -19,8 +20,8 @@ const STALEMATE: u8 = 2;
 const VICTORY: u8 = 3;
 
 /// Colors
-const WHITE: u8 = 1;
-const BLACK: u8 = 2;
+const WHITE: u8 = 0;
+const BLACK: u8 = 1;
 
 /// Piece types
 const PAWN: u8 = 1;
@@ -34,11 +35,11 @@ sol_storage! {
   #[entrypoint]
   pub struct StylusChess {
     /// Total games of chess started
-    uint256 total_games;
+    uint64 total_games;
     /// Used to store a single pending game while waiting for a player two to join.
-    uint256 pending_game;
+    uint64 pending_game;
     /// Stores info for each chess game
-    mapping(uint256 => GameInfo) games;
+    mapping(uint64 => GameInfo) games;
   }
 
   pub struct GameInfo {
@@ -51,37 +52,41 @@ sol_storage! {
     /// 1 = WHITE; 2 = BLACK
     uint8 victor;
     /// All the info needed to rebuild the board
-    StorageChessPiece[8][8] pieces;
+    uint256 board_state;
   }
 
-  pub struct StorageChessPiece {
-    /// 1 for WHITE; 2 for BLACK
-    uint8 color;
-    /// 1 = Pawn, 2 = Knight, 3 = Bishop, 4 = Rook, 5 = Queen, 6 = King
-    uint8 piece_type;
-  }
+  // pub struct StorageChessPiece {
+  //   /// 1 for WHITE; 2 for BLACK
+  //   uint1 color;
+  //   /// 1 = Pawn, 2 = Knight, 3 = Bishop, 4 = Rook, 5 = Queen, 6 = King
+  //   uint3 piece_type;
+  // }
 }
+
+// sol! {
+//   pub struct GameInfoReturnType {}
+// }
 
 type GameInfoReturnType = (Address, Address, U256, U256);
 type GamePiecesReturnType = Vec<(U256, U256, U256, U256)>;
 
-struct ChessPiece {
-    color: U8,
-    row: U8,
-    col: U8,
-    piece_type: U8,
-}
+// struct ChessPiece {
+//     color: u8,
+//     row: u8,
+//     col: u8,
+//     piece_type: u8,
+// }
 
 #[external]
 impl StylusChess {
     /// Gets the game number from storage.
-    pub fn total_games(&self) -> Result<U256, Vec<u8>> {
-        Ok(self.total_games.get())
-    }
+    // pub fn total_games(&self) -> Result<U64, Vec<u8>> {
+    //     Ok(self.total_games.get())
+    // }
 
     /// Game info
     pub fn game_by_number(&self, game_number: U256) -> Result<GameInfoReturnType, Vec<u8>> {
-        let game_info = self.games.getter(game_number);
+        let game_info = self.games.getter(U64::from(game_number));
 
         let player_one = game_info.player_one.get();
         let player_two = game_info.player_two.get();
@@ -99,31 +104,9 @@ impl StylusChess {
     }
 
     /// The board layout for a particular game
-    pub fn game_pieces_by_game_number(
-        &self,
-        game_number: U256,
-    ) -> Result<GamePiecesReturnType, Vec<u8>> {
-        let game_info = self.games.getter(game_number);
-
-        // let pieces_length = game_info.pieces.len();
-        let mut pieces = GamePiecesReturnType::new();
-
-        for row in 0..8_u8 {
-            for col in 0..8_u8 {
-                if let Some(row_vec) = game_info.pieces.getter(row) {
-                    if let Some(piece) = row_vec.getter(col) {
-                        pieces.push((
-                            U256::from(piece.color.get()),
-                            U256::from(row),
-                            U256::from(col),
-                            U256::from(piece.piece_type.get()),
-                        ));
-                    }
-                }
-            }
-        }
-
-        Ok(pieces)
+    pub fn game_pieces_by_game_number(&self, game_number: U256) -> Result<U256, Vec<u8>> {
+        let game_info = self.games.get(U64::from(game_number));
+        Ok(game_info.board_state.get())
     }
 
     /// Either creates a new game or joins a pending game if it exists
@@ -132,92 +115,85 @@ impl StylusChess {
         let pending_game = self.pending_game.get();
 
         // Sets pending_game number and initializes a new board
-        if pending_game == U256::from(0) {
+        if pending_game == U64::from(0) {
             let game_number = self.get_next_game_number();
             self.pending_game.set(game_number);
             self.create_game(game_number);
-            return Ok(game_number);
+            return Ok(U256::from(game_number));
         }
 
         self.join_game(pending_game);
 
-        Ok(pending_game)
+        Ok(U256::from(pending_game))
     }
 }
 
 impl StylusChess {
-    fn get_next_game_number(&mut self) -> U256 {
-        let game_number = self.total_games.get() + U256::from(1);
+    fn get_next_game_number(&mut self) -> U64 {
+        let game_number = self.total_games.get() + U64::from(1);
         self.total_games.set(game_number);
         game_number
     }
 
-    fn create_game(&mut self, game_number: U256) {
+    fn create_game(&mut self, game_number: U64) {
         let board = Board::default();
         // Set up pieces for serialization
-        let pieces = self.serialize_board(board);
+        let board_state = self.serialize_board(board);
 
         let mut game_info = self.games.setter(game_number);
         game_info.player_one.set(msg::sender());
-
-        for piece in pieces {
-            let row = piece.row;
-            let col = piece.col;
-
-            if let Some(mut row_vec) = game_info.pieces.setter(row) {
-                if let Some(mut chess_piece) = row_vec.setter(col) {
-                    chess_piece.color.set(piece.color);
-                    chess_piece.piece_type.set(piece.piece_type);
-                }
-            }
-        }
+        game_info.board_state.set(board_state);
     }
 
-    fn join_game(&mut self, game_number: U256) {
+    fn join_game(&mut self, game_number: U64) {
         let mut game_info = self.games.setter(game_number);
         // join as player two
         game_info.player_two.set(msg::sender());
         // change status to continuing
         game_info.game_status.set(U8::from(CONTINUING));
         // empty out pending_game
-        self.pending_game.set(U256::from(0));
+        self.pending_game.set(U64::from(0));
     }
 
-    fn serialize_board(&self, board: Board) -> Vec<ChessPiece> {
-        let mut chess_pieces = Vec::<ChessPiece>::new();
+    // fn deserialize_board(&self, board_state: U256) -> Board {
 
-        for row in 0..8 {
-            for col in 0..8 {
-                let position = Position::new(row, col);
+    // }
+
+    fn serialize_board(&self, board: Board) -> U256 {
+        let mut board_state = U256::from(0);
+
+        for row in 0..8_u8 {
+            for col in 0..8_u8 {
+                let position = Position::new(row.into(), col.into());
+                let base_offset: usize = ((row * 8 + col) * 4).into();
+                let color_offset: usize = base_offset + 3;
+                let piece_type_offset: usize = base_offset;
+
                 if let Some(board_piece) = board.get_piece(position) {
                     let color = if board_piece.get_color() == Color::White {
-                        U8::from(WHITE)
+                        WHITE
                     } else {
-                        U8::from(BLACK)
+                        BLACK
                     };
 
                     let piece_type = match board_piece {
-                        Piece::Pawn(_, _) => U8::from(PAWN),
-                        Piece::Knight(_, _) => U8::from(KNIGHT),
-                        Piece::Bishop(_, _) => U8::from(BISHOP),
-                        Piece::Rook(_, _) => U8::from(ROOK),
-                        Piece::Queen(_, _) => U8::from(QUEEN),
-                        Piece::King(_, _) => U8::from(KING),
+                        Piece::Pawn(_, _) => PAWN,
+                        Piece::Knight(_, _) => KNIGHT,
+                        Piece::Bishop(_, _) => BISHOP,
+                        Piece::Rook(_, _) => ROOK,
+                        Piece::Queen(_, _) => QUEEN,
+                        Piece::King(_, _) => KING,
                     };
 
-                    let piece_tuple = ChessPiece {
-                        color,
-                        row: U8::from(row),
-                        col: U8::from(col),
-                        piece_type,
-                    };
-
-                    chess_pieces.push(piece_tuple)
+                    board_state |= U256::from(color) << color_offset;
+                    board_state |= U256::from(piece_type) << piece_type_offset;
+                } else {
+                    board_state |= U256::from(0) << (base_offset + 4);
                 }
             }
         }
 
-        chess_pieces
+        board_state
     }
 }
 
